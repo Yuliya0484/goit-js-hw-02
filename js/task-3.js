5a140d77c99da7c4a59609e0bd9b7d4fbaa85431
Yuliya Bondarenko <juliyaodessa84@gmail.com>
2024-09-01 10:40:59 +0200

    done

--- js/task-3.js
+++ js/task-3.js
@@ -1,8 +1,19 @@
 "use strict";
-function getElementWidth(content, padding, border) {
-  return parseFloat(content) + (parseFloat(padding) + parseFloat(border)) * 2;
+
+// function checkForSpam(message) {
+//   // const message = ("sale", "spam");
+//   return message.toLowerCase() === sale || spam ? "true" : "false";
+// }
+
+function checkForSpam(message) {
+  const lowerCaseMessage = message.toLowerCase();
+  return lowerCaseMessage.includes("spam") || lowerCaseMessage.includes("sale");
 }
 
-console.log(getElementWidth("50px", "8px", "4px"));
-console.log(getElementWidth("60px", "12px", "8.5px"));
-console.log(getElementWidth("200px", "0px", "0px"));
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
